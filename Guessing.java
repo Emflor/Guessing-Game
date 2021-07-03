2573bc9be750b1482572f41f805fc3e6e1fe4f56
Emflor <86896801+Emflor@users.noreply.github.com>
2021-07-03 16:05:52 -0700

    initial commit

---
+++ Guessing.java
@@ -0,0 +1,52 @@
+import java.math.MathContext;
+import java.util.Scanner;
+
+public class Guessing {
+
+    static int computerGuess(final int lowval, final int highval, final int randnum, int count){
+        if (highval >= lowval) {
+            final int guess = lowval + (highval - lowval) / 2;
+
+            if (guess == randnum) {
+                return count;
+            }
+                if (guess > randnum) {
+                    count++;
+                    return (computerGuess(lowval, guess-1, randnum, count));
+                } else {
+                    count++;
+                    return (computerGuess(guess+1, highval, randnum, count));
+                }
+        }   else {
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        int guess = -99;
+        int count = 0;
+        final Scanner input = new Scanner(System.in);
+        System.out.print("Enter a guess between 1 and 100:");
+        guess = input.nextInt();
+
+        // generate random number between 1 and 100
+        final int randnum = (int) (Math.random() * 100 + 1);
+
+        while (guess != randnum) {
+
+            System.out.print("Enter a guess between 1 and 100:");
+            guess = input.nextInt();
+            if (guess < randnum) {
+                System.out.println("higher");
+            } else if (guess > randnum) {
+                System.out.println("lower");
+            } else {
+                System.out.println("You guessed it!!");
+                break;
+            }
+            count++;
+        } // end while loop
+        input.close();
+        System.out.println("You took " + count + " steps to guess the number.");
+        System.out.println("The computer took " + computerGuess(0, 100, randnum, 0)+ " to guess the number.");
+    }
+}
